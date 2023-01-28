e1404c426973ac5861b455bc0a9309136e32a36f
Jovan <jovanivko@gmail.com>
2023-01-28 06:11:27 +0100

    Finished AB+ syntax spec without error recovery, accidently deleted lexer spec. TODO rewrite

---
+++ workspace/MJCompiler/src/rs/ac/bg/etf/pp1/ast/ConstDecl.java
@@ -0,0 +1,109 @@
+// generated with ast extension for cup
+// version 0.8
+// 28/0/2023 6:7:54
+
+
+package rs.ac.bg.etf.pp1.ast;
+
+public class ConstDecl implements SyntaxNode {
+
+    private SyntaxNode parent;
+    private int line;
+    private Type Type;
+    private String constName;
+    private ConstVal ConstVal;
+
+    public ConstDecl (Type Type, String constName, ConstVal ConstVal) {
+        this.Type=Type;
+        if(Type!=null) Type.setParent(this);
+        this.constName=constName;
+        this.ConstVal=ConstVal;
+        if(ConstVal!=null) ConstVal.setParent(this);
+    }
+
+    public Type getType() {
+        return Type;
+    }
+
+    public void setType(Type Type) {
+        this.Type=Type;
+    }
+
+    public String getConstName() {
+        return constName;
+    }
+
+    public void setConstName(String constName) {
+        this.constName=constName;
+    }
+
+    public ConstVal getConstVal() {
+        return ConstVal;
+    }
+
+    public void setConstVal(ConstVal ConstVal) {
+        this.ConstVal=ConstVal;
+    }
+
+    public SyntaxNode getParent() {
+        return parent;
+    }
+
+    public void setParent(SyntaxNode parent) {
+        this.parent=parent;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public void setLine(int line) {
+        this.line=line;
+    }
+
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+
+    public void childrenAccept(Visitor visitor) {
+        if(Type!=null) Type.accept(visitor);
+        if(ConstVal!=null) ConstVal.accept(visitor);
+    }
+
+    public void traverseTopDown(Visitor visitor) {
+        accept(visitor);
+        if(Type!=null) Type.traverseTopDown(visitor);
+        if(ConstVal!=null) ConstVal.traverseTopDown(visitor);
+    }
+
+    public void traverseBottomUp(Visitor visitor) {
+        if(Type!=null) Type.traverseBottomUp(visitor);
+        if(ConstVal!=null) ConstVal.traverseBottomUp(visitor);
+        accept(visitor);
+    }
+
+    public String toString(String tab) {
+        StringBuffer buffer=new StringBuffer();
+        buffer.append(tab);
+        buffer.append("ConstDecl(\n");
+
+        if(Type!=null)
+            buffer.append(Type.toString("  "+tab));
+        else
+            buffer.append(tab+"  null");
+        buffer.append("\n");
+
+        buffer.append(" "+tab+constName);
+        buffer.append("\n");
+
+        if(ConstVal!=null)
+            buffer.append(ConstVal.toString("  "+tab));
+        else
+            buffer.append(tab+"  null");
+        buffer.append("\n");
+
+        buffer.append(tab);
+        buffer.append(") [ConstDecl]");
+        return buffer.toString();
+    }
+}
